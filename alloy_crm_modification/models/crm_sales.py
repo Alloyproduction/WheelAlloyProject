26b966d40a1dc7a0d1d038df76ff1d4bbf418ac5
Alloyproduction <49244449+Alloyproduction@users.noreply.github.com>
2020-05-16 15:27:43 +0300

    Add files via upload

--- alloy_crm_modification/models/crm_sales.py
+++ alloy_crm_modification/models/crm_sales.py
@@ -323,6 +323,28 @@ class SalesOrdercrm(models.Model):
     #         print("hi ....?????")
     #         # self.send_m("New Quotation for Follow up","New Quotation for Follow up"+str(self.name),user_Obj)
 
+    # @api.multi
+    # def action_confirm(self):
+    #     if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+    #         raise UserError(_(
+    #             'It is not allowed to confirm an order in the following states: %s'
+    #         ) % (', '.join(self._get_forbidden_state_confirm())))
+    #
+    #     for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+    #         order.message_subscribe([order.partner_id.id])
+    #     self.write({
+    #         'state': 'sale',
+    #         'confirmation_date': fields.Datetime.now()
+    #     })
+    #     self._action_confirm()
+    #     if self.env['ir.config_parameter'].sudo().get_param('sale.auto_done_setting'):
+    #         self.action_done()
+    #     return True
+    #
+    @api.multi
+    def action_confirm(self):
+
+        return True
 
     def send_salesman(self):
         return {
