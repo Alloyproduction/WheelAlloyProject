d634fe4c3d86df761acf25753dc61241a789ff71
Alloyproduction <49244449+Alloyproduction@users.noreply.github.com>
2020-09-14 15:47:59 +0300

    Add files via upload

--- journal_entries_print/models/partner.py
+++ journal_entries_print/models/partner.py
@@ -1,46 +1,50 @@
 from odoo import fields, models
 
-# _CLAIMSTATES = [
-#     ('stat_draft', 'Draft'),
-#     ('stat_confirmed', 'Confirmed'),
-#     ('stat_process', 'process'),
-#     ('stat_won', 'won'),
-#     ('stat_lost', 'lost'),
-# ]
 
 class Partner(models.Model):
     _name = "account.partner"
-    _inherit = ['account.bank.statement', 'mail.thread', 'mail.activity.mixin']
+    _inherit = ['account.bank.statement', 'purchase.order', 'mail.thread', 'mail.activity.mixin']
+
+    # po_number = fields.Char('Next Action')
+    po_number = fields.Many2one('purchase.order',
+                                ondelete='set null', string="PO Number", index=True)
+    _CLAIMSTATES = [
+        ('stat_draft', 'Draft'),
+        ('stat_confirmed', 'Confirmed'),
+        ('stat_process', 'process'),
+        ('stat_won', 'won'),
+        ('stat_lost', 'lost'),
+    ]
+
+    state = fields.Selection(selection=_CLAIMSTATES)
 
 
-    po_number = fields.Char('Next Action')
-    # state = fields.Selection(selection=_CLAIMSTATES,
-    #                          string='Status',
-    #                          track_visibility='onchange',
-    #                          required=True,
-    #                          copy=False,
-    #                          default='stat_draft')
 
 
-# @api.multi
-# def action_draft(self):
-#     # self.state = 'stat_draft'
-#     return self.write({'state': 'stat_draft'})
-#     self.ldate = datetime.now()
-#
-#
-# # @api.multi
-# # def action_confirm(self):
-# #     # self.state = 'stat_confirmed'
-# #     return self.write({'state': 'stat_confirmed'})
-# #     self.ldate = datetime.now()
-#
-#
-# @api.multi
-# def action_process(self):
-#     # self.state = 'stat_process'
-#     return self.write({'state': 'stat_process'})
-#     self.ldate = datetime.now()
+
+@api.multi
+def action_draft(self):
+    self.state = 'stat_draft'
+    return self.write({'state': 'stat_draft'})
+    self.ldate = datetime.now()
+
+
+@api.multi
+def action_confirm(self):
+    res = super(Partner, self).action_confirm()
+    res_conf = self.env['res.config.settings'].sudo()
+    self.state = 'stat_confirmed'
+    return self.write({'state': 'stat_confirmed'})
+    self.ldate = datetime.now()
+
+
+@api.multi
+def action_process(self):
+    res = super(Partner, self).action_process()
+    res_conf = self.env['res.config.settings'].sudo()
+    self.state = 'stat_process'
+    return self.write({'state': 'stat_process'})
+    self.ldate = datetime.now()
 
 
 class journal_post_button(models.Model):
