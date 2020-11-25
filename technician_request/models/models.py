9d3409612d8b9f75ecd1e1c6307b866674eacf1c
Alloyproduction <49244449+Alloyproduction@users.noreply.github.com>
2020-11-25 15:20:35 +0300

    Add files via upload

--- technician_request/models/models.py
+++ technician_request/models/models.py
@@ -2,8 +2,7 @@
 from odoo import api, fields, models, _ , SUPERUSER_ID
 from odoo.addons import decimal_precision as dp
 from datetime import datetime
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, UserError, float_is_zero, float_compare
 
 _REQUESTSTATES = [
     ('draft', 'Draft'),
@@ -237,26 +236,136 @@ class TechicianRequest(models.Model):
 class stockpickUser(models.Model):
     _inherit = 'stock.picking'
 
+    def _check_sms_confirmation_popup(self):
+        return False
+
+    def button_validate(self):
+        self.ensure_one()
+        if not self.move_lines and not self.move_line_ids:
+            raise UserError(_('Please add some items to move.'))
+
+        print("hi..>>..??..??222322")
+
+        # Clean-up the context key at validation to avoid forcing the creation of immediate
+        # transfers.
+        ctx = dict(self.env.context)
+        ctx.pop('default_immediate_transfer', None)
+        self = self.with_context(ctx)
+
+        # add user as a follower
+        self.message_subscribe([self.env.user.partner_id.id])
+        print("hi..>>..??..??222")
+
+        # If no lots when needed, raise error
+        picking_type = self.picking_type_id
+        precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        no_quantities_done = all(float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in self.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel')))
+        no_reserved_quantities = all(float_is_zero(move_line.product_qty, precision_rounding=move_line.product_uom_id.rounding) for move_line in self.move_line_ids)
+        if no_reserved_quantities and no_quantities_done:
+            raise UserError(_('You cannot validate a transfer if no quantites are reserved nor done. To force the transfer, switch in edit more and encode the done quantities.'))
+
+        print("hi..>>..??..??")
+        # for i in self.location_dest_id.manager_ids:
+        #     print(i.id)
+        recipient_partners = []
+        if self.location_dest_id.manager_ids:
+            for i in self.location_dest_id.manager_ids:
+                recipient_partners.append(i.id)
+            print('Testing Func0099999')
+            print('Testing Func004')
+        if len(recipient_partners):
+            print('Testing Func005')
+            self.message_post(body="This Purchase Is Validated",
+                              subtype='mail.mt_comment',
+                              subject="Purchase Validated [ " + str(self.name) + " ]",
+                              partner_ids=recipient_partners,
+                              message_type='comment')
+
+        if picking_type.use_create_lots or picking_type.use_existing_lots:
+            lines_to_check = self.move_line_ids
+            if not no_quantities_done:
+                lines_to_check = lines_to_check.filtered(
+                    lambda line: float_compare(line.qty_done, 0,
+                                               precision_rounding=line.product_uom_id.rounding)
+                )
+
+            for line in lines_to_check:
+                product = line.product_id
+                if product and product.tracking != 'none':
+                    if not line.lot_name and not line.lot_id:
+                        raise UserError(_('You need to supply a Lot/Serial number for product %s.') % product.display_name)
+
+
+        # Propose to use the sms mechanism the first time a delivery
+        # picking is validated. Whatever the user's decision (use it or not),
+        # the method button_validate is called again (except if it's cancel),
+        # so the checks are made twice in that case, but the flow is not broken
+        sms_confirmation = self._check_sms_confirmation_popup()
+        if sms_confirmation:
+            return sms_confirmation
+
+        if no_quantities_done:
+            view = self.env.ref('stock.view_immediate_transfer')
+            wiz = self.env['stock.immediate.transfer'].create({'pick_ids': [(4, self.id)]})
+            return {
+                'name': _('Immediate Transfer?'),
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'stock.immediate.transfer',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'res_id': wiz.id,
+                'context': self.env.context,
+            }
+
+        if self._get_overprocessed_stock_moves() and not self._context.get('skip_overprocessed_check'):
+            view = self.env.ref('stock.view_overprocessed_transfer')
+            wiz = self.env['stock.overprocessed.transfer'].create({'picking_id': self.id})
+            return {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'stock.overprocessed.transfer',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'res_id': wiz.id,
+                'context': self.env.context,
+            }
+
+
+        # Check backorder should check for other barcodes
+        if self._check_backorder():
+            return self.action_generate_backorder_wizard()
+        self.action_done()
+        return
+
+
+    def action_generate_backorder_wizard(self):
+        view = self.env.ref('stock.view_backorder_confirmation')
+        wiz = self.env['stock.backorder.confirmation'].create({'pick_ids': [(4, p.id) for p in self]})
+        return {
+            'name': _('Create Backorder?'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'stock.backorder.confirmation',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'res_id': wiz.id,
+            'context': self.env.context,
+        }
+
+
+
     @api.model
     def _get_default_manager_by(self):
         return self.env['res.users'].browse(self.env.uid)
 
-    source_request = fields.Many2one('tech.request',
-                                       'Source Request',
+    source_request = fields.Many2one('tech.request','Source Request',track_visibility='onchange',readonly=True)
 
-                                       track_visibility='onchange',
-                                       readonly=True)
-
-    employee_user_id= fields.Many2one('res.users',
-                                 'Employee Request',
-
-                                 track_visibility='onchange',
-                                    readonly=True)
-    manager_by = fields.Many2one('res.users',
-                                   'Manage by',
-
-                                   track_visibility='onchange',
-                                   default=_get_default_manager_by, readonly=True)
+    employee_user_id= fields.Many2one('res.users','Employee Request',track_visibility='onchange',readonly=True)
+    manager_by = fields.Many2one('res.users','Manage by',track_visibility='onchange',default=_get_default_manager_by, readonly=True)
 
     @api.onchange('picking_type_id', 'partner_id')
     def onchange_picking_type(self):
@@ -312,8 +421,6 @@ class stocklocationmanager(models.Model):
     _inherit = "stock.location"
 
     manager_ids = fields.Many2many('res.users',  string='Manager of Location')
-
-
 
 
 class TechnicianRequestLine(models.Model):
