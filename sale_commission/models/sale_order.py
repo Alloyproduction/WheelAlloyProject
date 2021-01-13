68fae9ffe451364320ed3b7c2fa27d32c666b1af
Alloyproduction <49244449+Alloyproduction@users.noreply.github.com>
2021-01-13 11:59:39 +0300

    Add files via upload

--- sale_commission/models/sale_order.py
+++ sale_commission/models/sale_order.py
@@ -1,7 +1,8 @@
-# Copyright 2014-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2014-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from lxml import etree
 
 
 class SaleOrder(models.Model):
@@ -23,6 +24,29 @@ class SaleOrder(models.Model):
     def recompute_lines_agents(self):
         self.mapped('order_line').recompute_agents()
 
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
+                        submenu=False):
+        """Add to the existing context of the field `order_line` "partner_id"
+        key for avoiding to be replaced by other view inheritance.
+
+        We have to do this processing in text mode without evaling context, as
+        it can contain JS stuff.
+        """
+        res = super(SaleOrder, self).fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar,
+            submenu=submenu,
+        )
+        if view_type == 'form':
+            doc = etree.XML(res['arch'])
+            for node in doc.xpath("//field[@name='order_line']"):
+                node_val = node.get('context', '{}').strip()[1:-1]
+                elems = node_val.split(',') if node_val else []
+                to_add = ["'partner_id': partner_id"]
+                node.set('context', '{' + ', '.join(elems + to_add) + '}')
+            res['arch'] = etree.tostring(doc)
+        return res
+
 
 class SaleOrderLine(models.Model):
     _inherit = [
@@ -42,18 +66,17 @@ class SaleOrderLine(models.Model):
         # We use this form as this is the way it's returned when no real vals
         agents_vals = vals.get('agents', [(6, 0, [])])
         if agents_vals and agents_vals[0][0] == 6 and not agents_vals[0][2]:
-            order = self.env['sale.order'].browse(vals['order_id'])
-            vals['agents'] = self._prepare_agents_vals_partner(
-                order.partner_id,
-            )
+            vals['agents'] = self._prepare_agents_vals(vals=vals)
         return super().create(vals)
 
-    def _prepare_agents_vals(self):
-        self.ensure_one()
-        res = super()._prepare_agents_vals()
-        return res + self._prepare_agents_vals_partner(
-            self.order_id.partner_id,
-        )
+    def _prepare_agents_vals(self, vals=None):
+        res = super()._prepare_agents_vals(vals=vals)
+        if self:
+            partner = self.order_id.partner_id
+        else:
+            order = self.env['sale.order'].browse(vals['order_id'])
+            partner = order.partner_id
+        return res + self._prepare_agents_vals_partner(partner)
 
     def _prepare_invoice_line(self, qty):
         vals = super(SaleOrderLine, self)._prepare_invoice_line(qty)
