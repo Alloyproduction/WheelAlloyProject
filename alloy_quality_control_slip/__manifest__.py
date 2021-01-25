02b0f2f4f7a225d83727b9e088cb900b9dce3c85
Alloyproduction <49244449+Alloyproduction@users.noreply.github.com>
2021-01-25 10:00:33 +0300

    Add files via upload

--- alloy_quality_control_slip/__manifest__.py
+++ alloy_quality_control_slip/__manifest__.py
@@ -14,8 +14,10 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/sales_report.xml',
         'views/mail_notification.xml',
+        'views/email_templete.xml',
         'report/sales_report.xml',
     ],
 }
--- alloy_quality_control_slip/models/sales.py
+++ alloy_quality_control_slip/models/sales.py
@@ -26,7 +26,9 @@ class QualityControlSlip(models.Model):
     _name = 'quality.control.slip'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char()
+
+    name = fields.Char(string='Reference', required=True, copy=False,
+                           readonly=True, index=True, default=lambda self: _('New'))
     invoice_number = fields.Char()
     claim_no = fields.Char()
     license_plate = fields.Char()
@@ -38,11 +40,27 @@ class QualityControlSlip(models.Model):
     service_advisor = fields.Many2one('res.users')
     user_id = fields.Many2one('res.users', track_visibility='always',)
     sale_id = fields.Many2one('sale.order')
+    partner_id = fields.Many2one('res.partner', related='sale_id.partner_id')
+    company_id = fields.Many2one('res.company')
     alloy_digital_signature = fields.Binary(widget="signature")
     state = fields.Selection(string="state", selection=[('draft', 'Draft'),('accept', 'Accept'), ('deny', 'Deny')],
                              default='draft', track_visibility='always',)
     job_card = fields.Char()
 
+    type_name = fields.Char('Type Name', compute='_compute_type_name')
+
+    @api.multi
+    @api.depends('state')
+    def _compute_type_name(self):
+        for record in self:
+            record.type_name = _('Quotation') if record.state in ('draft', 'accept', 'deny') else _('Quality Control')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('quality.control.slip') or _('New')
+        result = super(QualityControlSlip, self).create(vals)
+        return result
 
     @api.multi
     def accept_qc_slip(self):
@@ -58,7 +76,6 @@ class QualityControlSlip(models.Model):
                         if line.stage_id.name == 'Delivery':  # line.stage_id.name == 'Finished and QC' or
                             line.is_delete_stage = True
 
-
     @api.multi
     def deny_qc_slip(self):
         for rec in self:
@@ -69,6 +86,61 @@ class QualityControlSlip(models.Model):
     def set_to_draft(self):
         for rec in self:
             rec.state = 'draft'
+
+    @api.multi
+    def action_send_qc(self):
+
+        '''
+        This function opens a window to compose an email, with the edi sale template message loaded by default
+        '''
+
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('alloy_quality_control_slip', 'qc_email_template')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        lang = self.env.context.get('lang')
+        template = template_id and self.env['mail.template'].browse(template_id)
+        if template and template.lang:
+            lang = template._render_template(template.lang, 'quality.control.slip', self.ids[0])
+        ctx = {
+            'default_model': 'quality.control.slip',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'model_description': self.with_context(lang=lang).type_name,
+            'custom_layout': "mail.mail_notification_borders",
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
+
+        # template_id = self.env.ref('alloy_quality_control_slip.qc_email_template').id
+        # template = self.env['mail.template'].browse(template_id)
+        # template.send_mail(self.id, force_send=True)
+        #
+        # print("Sending email")
+        # self.env['mail.template'].browse(template_id).send_mail(self.id, force_send=True)
+
+
+
+
 
     # @api.multi
     # @api.depends('state')
@@ -137,10 +209,11 @@ class SaleOrder(models.Model):
         qc_obj = self.env['quality.control.slip']
         for rec in self:
             qc_search_obj = qc_obj.search([('id','=',self.qc_slip_id.id)])
+            print("Reference")
+            print(qc_search_obj.name)
             if qc_search_obj:
                 print("done1")
                 qc_search_obj.update({
-                    'name': rec.name,
                     'invoice_number': rec.invoice_ids.number,
                     'claim_no': rec.claim_no,
                     'license_plate': rec.vehicle.license_plate,
@@ -168,8 +241,9 @@ class SaleOrder(models.Model):
                 print("done2")
                 for line in rec.order_line:
                     total_qty += line.product_uom_qty
+                print("Reference")
+                print(qc_search_obj.name)
                 qc_id = qc_obj.create({
-                  'name': rec.name,
                   'invoice_number': rec.invoice_ids.number,
                   'claim_no': rec.claim_no,
                   'license_plate': rec.vehicle.license_plate,
