fdee06f14455d946785b9b73e1c72c91be142912
Alloyproduction <49244449+Alloyproduction@users.noreply.github.com>
2021-12-13 16:06:08 +0300

    Add files via upload

--- uat_custom/models/models.py
+++ uat_custom/models/models.py
@@ -14,18 +14,28 @@ class Uatcustom(models.Model):
                                      compute='get_conv_hra')
     hra = fields.Monetary(string='Housing Allowance', tracking=True,
                           help="House rent allowance.", compute='get_conv_hra')
+    include_allowance = fields.Boolean(string="Include Allowance")
 
-    @api.onchange('wage', 'hra', 'travel_allowance', 'conv_allowance')
+    @api.onchange('wage', 'hra', 'conv_allowance', 'other_allowance')
     def get_total_custom(self):
+        other_allowance = 0
         for t in self:
             # print(t.new_total)
-            t.new_total = t.wage + t.hra + t.conv_allowance + t.travel_allowance
+            if self.include_allowance == True:
+                t.new_total = t.wage + t.hra + t.conv_allowance + t.other_allowance
+            else:
+                t.new_total = t.wage + t.other_allowance
+
             # print('new_total', t.new_total)
 
-    @api.onchange('wage')
+    @api.onchange('wage','include_allowance')
     def get_conv_hra(self):
         for h in self:
-            # print(h.hra)
-            h.hra = h.wage * 0.25
-            h.conv_allowance = h.wage * 0.10
-            # print('wwee', h.hra )
+            if self.include_allowance == True:
+                # print(h.hra)
+                h.hra = h.wage * 0.25
+                h.conv_allowance = h.wage * 0.10
+            else:
+               self.hra = 0
+               self.conv_allowance = 0
+                # print('wwee', h.hra )
